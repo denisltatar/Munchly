5cb301a0fd646f1df2dd24f7df4ed98c02d733fa
denisltatar <denis.tatar8@gmail.com>
2025-11-12 15:46:55 -0500

    app icon and loading image

---
+++ Munchly/Munchly/MunchlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchlyApp.swift
+//  Munchly
+//
+//  Created by Denis Tatar 2 on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MunchlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Munchly/MunchlyTests/MunchlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MunchlyTests.swift
+//  MunchlyTests
+//
+//  Created by Denis Tatar 2 on 11/12/25.
+//
+
+import Testing
+@testable import Munchly
+
+struct MunchlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
